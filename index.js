4021dfb88df10798c242d16d61634c9f074599bc
daaspro <daaspro.ru@yandex.ru>
2021-05-09 18:27:04 +0300

    feat: add function hsandler input params

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { program } = require('commander');
 const util = require('util');
+const fs = require("fs");
 
 const log = console.log;
+
+const apiInput = pathStr => {
+    if (fs.existsSync(pathStr)) return log('file is reading');
+    if (!pathStr) {
+        process.stdout.write("Type your input: ");
+        return process.stdin;
+    }
+    process.stderr.write("Input file does not found");
+    process.exit(1);
+}
 
 try {
     program
@@ -11,7 +22,8 @@ try {
         .option('-a, --action <value>', 'action encode/decode')
         .parse(process.argv);
     log(program.opts())
-    if (!program.opts().shift || !program.opts().actions) throw new Error('missing shift or action params')
+    if (!program.opts().shift || !program.opts().action) throw new Error('missing shift or action params')
+    apiInput(program.opts().input)
 } catch (err) {
     log(`Exception : \n${err.message}`)
     process.exit(1)
